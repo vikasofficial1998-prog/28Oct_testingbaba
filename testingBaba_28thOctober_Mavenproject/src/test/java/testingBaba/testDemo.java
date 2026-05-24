e642ec58c1d02337caf1eec3c4c4e50a27711944
vikasofficial1998-prog <vikas.official1998@gmail.com>
2026-05-24 12:45:52 +0530

    Add files via upload

---
+++ testingBaba_28thOctober_Mavenproject/src/test/java/testingBaba/testDemo.java
@@ -0,0 +1,19 @@
+package testingBaba;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class testDemo {
+	
+	@Test
+	public void lunchUrl() 
+	{
+
+		WebDriver driver = new EdgeDriver();
+	}
+
+}
